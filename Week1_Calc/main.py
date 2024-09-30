c4fc932854f347b157c5646431f0cf2488a45606
cjhosken <hoskenchristopher@gmail.com>
2024-09-30 15:00:31 +0100

    python working

--- Week1_Calc/main.py
+++ Week1_Calc/main.py
@@ -1,28 +1,20 @@
-from PIL import Image, ImageDraw
-import numpy as np
 import cv2
+import numpy as np
 
-# Step 1: Create a black image using Pillow
-width, height = 500, 500  # You can specify any size
-img = Image.new('RGB', (width, height), (0, 0, 0))
-
-# Draw a rectangle and a point
-draw = ImageDraw.Draw(img)
-draw.rectangle([(10, 10), (50, 50)], fill="#ffff33")  # Draw yellow rectangle
-draw.point(xy=(70, 70), fill="white")  # Draw white point
-
-def show_and_save(name, img):
-    # Convert the Pillow image to a NumPy array
-    im = np.array(img)
+def main():
+    image = np.zeros((512, 512, 3), dtype=np.uint8)
     
-    # Convert the image from RGB (Pillow) to BGR (OpenCV)
-    im = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
-    
-    # Show the image with OpenCV
-    cv2.imshow(name, im)
+    cv2.rectangle(image, (50, 50), (250, 150), (0, 255, 0), -1) # (img, tl, br, color, thickness)
+
+    image[35, 100] = (0, 0, 255)
+
+    # Step 5: Display the image
+    cv2.imshow('Image', image)
+    cv2.imwrite('image.png', image)
 
     # Wait for a key press and close the window
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-show_and_save("yolo", img)
+if __name__ == "__main__":
+    main()
