9f720ca1c59b4eb1cb31e52a80f8a33a2a580aeb
cjhosken <hoskenchristopher@gmail.com>
2024-10-03 22:09:14 +0100

    working week1

--- AM_module/AM_Canvas.py
+++ AM_module/AM_Canvas.py
@@ -13,16 +13,10 @@ class AM_Canvas:
         self.canvas=ImageDraw.Draw(self.image)
 
     def draw(self):
-        self.draw_grid()
         pass
-
-    def draw_grid(self, spacing=50, color=(50, 50, 50), axis_color=(100, 100, 100)):
-        self.canvas.line([(0,self.height/2),(self.width,self.height/2)],axis_color)
-        self.canvas.line([(self.width/2,0),(self.width/2,self.height)],axis_color)
 
     def show(self):
         self.image.show()
     
     def save(self, name="image.png"):
-        out_image = self.image.transpose(Image.FLIP_TOP_BOTTOM)
-        out_image.save(name)
+        self.image.save(name)
--- Week1_Calc/main.py
+++ Week1_Calc/main.py
@@ -1,58 +1,60 @@
 #!/usr/bin/env python
-from PIL import Image, ImageDraw
-from ..AM_module.AM_Canvas import AM_Canvas
+import AM_module.AM_Canvas as am 
+from PIL import Image
+from math import *
+
 
 def f(x):
-    return x**3
+    return sin(x) * 100
 
 def df(x, dt):
     return (f(x+dt) - f(x)) / dt
 
 def F(x, dt):
-    return dt * (f(x) + f(x + 1)) / 2
+    return (dt / 2) * (f(x) + f(x + dt))
 
-#!/usr/bin/env python
-from PIL import Image, ImageDraw
-import AM_module.AM_Canvas as am 
 
 class AM_Canvas(am.AM_Canvas):
     def draw(self):
         super().draw()
 
-        Y_INTERCEPT = WIDTH/2
-        X_INTERCEPT = HEIGHT/2
+        
 
-        dt = 0.01
-        AMP = 50
+        dt = 0.05
+        offset_y = self.height/2
 
         total = 0
-        theta = 0.01
 
-        for sx in range(int(-WIDTH/2), int(WIDTH/2)):       
-            x = sx * theta
+        self.canvas.line([(0, offset_y), (self.width, offset_y)], fill=(75, 75, 75))
 
+        for sx in range(self.width):       
+            # remapping x from pixel space to graph space
+            x = sx * dt
+
+            # Original function
             f0 = f(x)
-            f1 = f(x + theta) 
+            f1 = f(x + dt) 
 
-            canvas.line([(sx + Y_INTERCEPT, f0 * AMP + Y_INTERCEPT), (sx + WIDTH/2, f1 * AMP + X_INTERCEPT)], fill=(255, 255, 255))
+            self.canvas.line([(sx, f0 + offset_y), (sx + 1, f1 + offset_y)], fill=(255, 255, 255))
 
             # Derive it
-
             d0 = df(x, dt) 
-            d1 = df(x+theta, dt)
+            d1 = df(x+dt, dt)
 
-            canvas.line([(sx + WIDTH/2, d0 * AMP + Y_INTERCEPT), (sx + WIDTH/2, d1 * AMP + Y_INTERCEPT)], fill=(255, 0, 0))
+            self.canvas.line([(sx, d0 + offset_y), (sx + 1, d1 + offset_y)], fill=(255, 0, 0))
 
             # Integrate it
-
             i0 = F(x, dt)
+            i1 = F(x+dt, dt)
 
-            i1 = F(x+theta, dt) 
+            self.canvas.line([(sx, i0 + total + offset_y), (sx + 1, i1 + i0 + total + offset_y)], fill=(0, 255, 0))
 
-            canvas.line([(sx + WIDTH/2, i0 * AMP + Y_INTERCEPT), (sx + 1 + WIDTH/2, i1 * AMP + Y_INTERCEPT)], fill=(0, 255, 0))
+            total = i0 + total
+
+        self.image = self.image.transpose(Image.FLIP_TOP_BOTTOM)
 
 def main():
-    am_canvas = AM_Canvas(512, 512, (0, 0, 0))
+    am_canvas = AM_Canvas(1024, 1024, (0, 0, 0))
     am_canvas.draw()
     am_canvas.show()
     am_canvas.save("Week0_Prep/image.png")
