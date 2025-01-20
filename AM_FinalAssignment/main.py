e7e69f3d42ef47228268da0cdfdbed02a9ec5c49
cjhosken <hoskenchristopher@gmail.com>
2025-01-20 16:18:20 +0000

    fft

--- AM_FinalAssignment/main.py
+++ AM_FinalAssignment/main.py
@@ -5,12 +5,12 @@ from PIL import Image
 def main():
     # Load the image and convert it into a NumPy data array. 
     # We use a greyscale image
-    image = Image.open("./low.png").convert("L")
+    image = Image.open("./image.png").convert("L")
     arr = np.array(image, dtype=np.float64)
 
     dft_arr = np.zeros_like(arr, dtype=np.complex128)
     for idx, row in enumerate(arr):
-        dft_arr[idx] = dft(row, False)
+        dft_arr[idx] = fft(row, False)
         print(idx)
 
     im = Image.fromarray(np.real(dft_arr))
@@ -18,7 +18,7 @@ def main():
     
     idft_arr = np.zeros_like(dft_arr, dtype=np.complex128)
     for idx, row in enumerate(dft_arr):
-        idft_arr[idx] = dft(row, True)
+        idft_arr[idx] = fft(row, True)/len(idft_arr)
         print(idx)
     
 
@@ -27,23 +27,23 @@ def main():
     im.show()
     im.save("./output.png")
 
-
-def dft(arr, inv=False):
+def fft(arr, inv=False):
     N = len(arr)
     sign = 1 if inv else -1
     result = np.zeros(N, dtype=np.complex128)
 
-    for k in range(N):
-        total = 0
-        for n in range(N):
-            total += arr[n] * cmath.exp(sign * 2j * cmath.pi * k * n/N)
+    if N == 1:
+        result[0] = arr[0]
+    
+    else:
+        dft_evens = fft(arr[::2], inv) 
+        dft_odds = fft(arr[1::2], inv)
 
-        if inv:
-            total /= N
-
-        result[k] = total
-        
-
+        for k in range(N//2):
+            t = cmath.exp(sign * 2j * cmath.pi * k/N) * dft_odds[k]
+            result[k] = dft_evens[k] + t
+            result[k+N//2] = dft_evens[k] - t
+    
     return result
 
 main()
